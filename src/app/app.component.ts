8ecc42c0cb95c62dac2b866652bf0a97c962ce17
sokhna00 <dsokhnadiarra@ept.sn>
2021-07-29 11:33:48 +0000

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-crash';
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,40 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserModule, enableDebugTools } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ButtonComponent } from './components/button/button.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
+import { AddTaskComponent } from './components/add-task/add-task.component';
+import {FormsModule} from '@angular/forms';
+import { FooterComponent } from './components/footer/footer.component';
+import { RouterModule,Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
 
+
+const appRoutes:Routes=[
+  {path: '', component:TasksComponent},
+  {path: 'about', component:AboutComponent}
+
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ButtonComponent,
+    TasksComponent,
+    TaskItemComponent,
+    AddTaskComponent,
+    FooterComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule,
+    HttpClientModule,FormsModule,RouterModule.forRoot(appRoutes, {enableTracing:true}),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
